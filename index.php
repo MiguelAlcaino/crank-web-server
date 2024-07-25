128c0b0fc46237a971a2ace682dd1ec2b9657b47
Miguel Alcaino <miguel.alcaino@ogb.cl>
2024-07-25 13:05:59 -0400

    Fix server ip

--- index.php
+++ index.php
@@ -38,7 +38,7 @@ $http = new React\Http\HttpServer(function (Psr\Http\Message\ServerRequestInterf
     );
 });
 
-$socket = new React\Socket\SocketServer('127.0.0.1:80');
+$socket = new React\Socket\SocketServer('0.0.0.0:80');
 $http->listen($socket);
 
 echo "Server running at http://127.0.0.1:80" . PHP_EOL;
